fda7a12bb175371ffd5cd6c2af4f1a62e0054cf4
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2020-04-05 21:01:54 -0600

    Fixed new extensions.

--- KellySharp/DictionaryExtensions.cs
+++ KellySharp/DictionaryExtensions.cs
@@ -5,17 +5,22 @@ namespace KellySharp
 {
     public static class DictionaryExtensions
     {
-        public static void GetOrAdd<TKey, TValue>(
+        public static TValue GetOrAdd<TKey, TValue>(
             this IDictionary<TKey, TValue> dictionary,
             TKey key,
             TValue defaultValue = default)
             where TKey : notnull
         {
             if (!dictionary.TryGetValue(key, out var value))
-                dictionary.Add(key, defaultValue);
+            {
+                value = defaultValue;
+                dictionary.Add(key, value);
+            }
+
+            return value;
         }
 
-        public static void GetOrAdd<TKey, TValue>(
+        public static TValue GetOrAdd<TKey, TValue>(
             this IDictionary<TKey, TValue> dictionary,
             TKey key,
             Func<TValue> valueFactory)
@@ -26,9 +31,11 @@ namespace KellySharp
                 value = valueFactory.Invoke();
                 dictionary.Add(key, value);
             }
+
+            return value;
         }
 
-        public static void AlwaysGet<TKey, TValue>(
+        public static TValue AlwaysGet<TKey, TValue>(
             this IDictionary<TKey, TValue> dictionary,
             TKey key,
             Func<TValue> valueFactory)
@@ -45,15 +52,17 @@ namespace KellySharp
                 value = valueFactory.Invoke();
                 dictionary[key] = value;
             }
+
+            return value;
         }
 
-        public static void AlwaysGet<TKey, TValue>(
+        public static TValue AlwaysGet<TKey, TValue>(
             this IDictionary<TKey, TValue> dictionary,
             TKey key)
             where TKey : notnull
             where TValue : class, new()
         {
-            AlwaysGet(dictionary, key, () => new TValue());
+            return AlwaysGet(dictionary, key, () => new TValue());
         }
     }
 }
