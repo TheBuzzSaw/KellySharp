55ff0b05e8207b53d8d5a297885b3735e419b41f
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2020-09-06 11:31:56 -0600

    Interface experimentation

--- KonsoleApp/Program.cs
+++ KonsoleApp/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading;
@@ -17,13 +18,38 @@ namespace KonsoleApp
         int DoTheThing(int input);
     }
 
+    interface ISecondWidget : IWidget
+    {
+        int DoTheOtherThing(int input);
+    }
+
+
     class Program
     {
+        static void DumpMethods(Type type)
+        {
+            Console.WriteLine(type);
+            
+            var methods = new List<MethodInfo>(type.GetMethods());
+            foreach (var interfaceType in type.GetInterfaces())
+            {
+                Console.WriteLine(interfaceType);
+                methods.AddRange(interfaceType.GetMethods());
+            }
+            
+            foreach (var method in methods)
+                Console.WriteLine($"  - {method.Name} from {method.DeclaringType}");
+        }
+
         static void Main(string[] args)
         {
-            Expression<Func<IWidget, int, int>> e = (w, i) => w.DoTheThing(i);
-            Console.WriteLine(e);
-            Console.WriteLine(e.Body);
+            // Expression<Func<IWidget, int, int>> e = (w, i) => w.DoTheThing(i);
+            // Console.WriteLine(e);
+            // Console.WriteLine(e.Body);
+
+            DumpMethods(typeof(ISecondWidget));
+            DumpMethods(typeof(IDictionary<,>));
+            DumpMethods(typeof(IList<>));
         }
     }
 }
